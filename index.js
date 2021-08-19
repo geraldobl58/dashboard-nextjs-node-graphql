ba27dc84473863071bdaadf0ce71abaf77e64143
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-19 14:05:59 -0300

    :sparkles: Adding Types and Resolvers in the code

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const server = new ApolloServer();
+const typeDefs = gql`
+  type Course {
+    title: String
+    tecnology: String
+  }
+
+  type Query {
+    getCourses: [Course]
+  }
+`
+
+const courses = [
+  {
+    title: "Javascript Moderno",
+    tecnology: "ES6",
+  },
+  {
+    title: "Reactjs",
+    tecnology: "Library Javascript",
+  },
+  {
+    title: "Nodejs",
+    tecnology: "Librabry Backend",
+  },
+  {
+    title: "Nextjs",
+    tecnology: "Librabry for websites and seo",
+  }
+]
+
+const resolvers = {
+  Query: {
+    getCourses: () => courses
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 
 server.listen().then(({ url }) => {
-  console.log(`Server Running 🎉🎉🎉🎉🎉 ${url}`)
+  console.log(`Server Running ${url}`)
 })
