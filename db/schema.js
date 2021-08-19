a2adef67cc74638e7288111a068ccc3c603e1c5b
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-19 17:34:32 -0300

    :sparkles: Creating types, resolvers and schema

--- db/schema.js
+++ db/schema.js
@@ -14,6 +14,14 @@ const typeDefs = gql`
     token: String
   }
 
+  type Product {
+    id: ID
+    name: String
+    stock: Int
+    price: Float
+    created: String
+  }
+
   input UserInput {
     name: String!
     nickname: String!
@@ -26,13 +34,23 @@ const typeDefs = gql`
     password: String!
   }
 
+  input ProductInput {
+    name: String!
+    stock: Int!
+    price: Float!
+  }
+
   type Query {
     getUser(token: String!): User
   }
 
   type Mutation {
+    # Users
     newUser(input: UserInput): User
     authenticateUser(input: AuthenticateInput): Token
+
+    # Products
+    newProduct(input: ProductInput): Product
   }
 `
 
